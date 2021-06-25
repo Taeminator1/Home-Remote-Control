e2d8d814e6d4161b936463c9fbca0b540126fff3
Taemin <dbsxoals689@naver.com>
2021-06-26 04:05:33 +0900

    PersonalInfo 구조체 추가해서 개인 URL 문자열 따로 관리

---
+++ Xcode/HRC/PersonalInfo.swift
@@ -0,0 +1,12 @@
+//
+//  PersonalInfo.swift
+//  HRC
+//
+//  Created by 윤태민 on 6/26/21.
+//
+
+import Foundation
+
+struct PersonalInfo {
+    static let url: String = "http://"     // write your own url
+}
