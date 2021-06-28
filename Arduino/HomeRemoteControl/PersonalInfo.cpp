2872cee419477cbb5aca5173a63509b95c16f51b
Taemin <dbsxoals689@naver.com>
2021-06-28 19:56:17 +0900

    PersonalInfo.cpp 파일이 더 이상 commit 되지 않도록 cache 삭제

--- Arduino/HomeRemoteControl/PersonalInfo.cpp
+++
@@ -1,5 +0,0 @@
-#include "PersonalInfo.h"
-
-char PersonalInfo::url[] = "http://";            // Enter your wifi ID
-char PersonalInfo::wifi_id[] = "";               // Enter your wifi PW
-char PersonalInfo::wifi_pw[] = "";               // Enter your server URL
