81745cde17aabb09624760bea3d7f9be1ed8c08b
Taemin <dbsxoals689@naver.com>
2021-07-05 20:35:54 +0900

    .js 파일내에서 HTML 분리, 추후 파일로 분리 필요

--- Node.js/app.js
+++ Node.js/app.js
@@ -3,20 +3,84 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-const inPort = 80;            // 내부 포트 번호
+const inPort = ;            // 내부 포트 번호
  
-function templateHTML(title, gui, script) {
+function loadHTML(readStr) {
   return `
   <!doctype html>
     <html>
     <head>
-      <title>${title}</title>
+      <title>Hmoe Remote Contorl</title>
       <meta charset="utf-8">
     </head>
     <body>
-      ${gui}
+      <form action="/create" method="post">
+        <h3>Test Buttons</h3>
+        <ul>
+          <li>
+            <label><input type="checkbox" name="test1" id="btn1"> button1 </label>
+            <label id = "label1"> ${readStr[0]} </label>
+          </li>
+          <li>
+            <label><input type="checkbox" name="test2" id="btn2"> button2 </label>
+            <label id = "label2"> ${readStr[1]} </label>
+          </li>
+            <p><input type="hidden" id="save"></p>
+        </ul>
+      </form>
       <script>
-        ${script}
+        var writtenStr = ["", ""];
+        writtenStr[0] = ${readStr[0]};
+        writtenStr[1] = ${readStr[1]};
+
+        // 웹페이지가 로드될 때 실행
+        window.onload = function() {
+          document.getElementById('btn1').checked = ${readStr[0]};
+          document.getElementById('btn2').checked = ${readStr[1]};
+
+          if(document.getElementById('btn1').checked) {
+            document.getElementById('label1').innerHTML = 'on';
+          } else {
+            document.getElementById('label1').innerHTML = 'off';
+          }
+
+          if(document.getElementById('btn2').checked) {
+            document.getElementById('label2').innerHTML = 'on';
+          } else {
+            document.getElementById('label2').innerHTML = 'off';
+          }
+        }
+
+        // btn1의 상태값 변경시키는 EventListener
+        btn1.addEventListener('click', (event) => {    
+          if(document.getElementById('btn1').checked) {
+            document.getElementById('label1').innerHTML = 'on'; 
+            writtenStr[0] = "true";
+          } else {
+            document.getElementById('label1').innerHTML = 'off'; 
+            writtenStr[0] = "false";
+          }
+          document.getElementById('save').form.submit();            // checkbox 누를 때마다 상태 save
+        });
+        // btn2의 상태값 변경시키는 EventListener
+        btn2.addEventListener('click', (event) => {    
+          if(document.getElementById('btn2').checked) {
+            document.getElementById('label2').innerHTML = 'on'; 
+            writtenStr[1] = "true";
+          } else {
+            document.getElementById('label2').innerHTML = 'off'; 
+            writtenStr[1] = "false";
+          }
+          document.getElementById('save').form.submit();            // checkbox 누를 때마다 상태 save
+        });
+
+        // Javascript Function
+        function btn1ButtonClicked() {
+          document.getElementById('btn1').click();
+        }
+        function btn2ButtonClicked() {
+          document.getElementById('btn2').click();
+        }
       </script>
     </body>
     </html>
@@ -30,80 +94,8 @@ var app = http.createServer(function(request,response){
         fs.readFile('data.txt', 'utf8', function(err, description){
             var readStr = description.split("/");
             console.log("Success reading file")
-            var template = templateHTML('Hmoe Remote Contorl', `
-              <!-----------------------------HTML GUI---------------------------->
-              <form action="/create" method="post">
-                  <h3>Test Buttons</h3>
-                  <ul>
-                      <li>
-                          <label><input type="checkbox" name="test1" id="btn1"> button1 </label>
-                          <label id = "label1"> ${readStr[0]} </label>
-                      </li>
-                      <li>
-                          <label><input type="checkbox" name="test2" id="btn2"> button2 </label>
-                          <label id = "label2"> ${readStr[1]} </label>
-                      </li>
-                      <p><input type="hidden" id="save"></p>
-                  </ul>
-              </form>
-              <!-----------------------------HTML GUI---------------------------->`, `
-              //----------------------------JAVASCRIPT----------------------------
-              var writtenStr = ["", ""];
-              writtenStr[0] = ${readStr[0]};
-              writtenStr[1] = ${readStr[1]};
-
-              // 웹페이지가 로드될 때 실행
-              window.onload = function() {
-                document.getElementById('btn1').checked = ${readStr[0]};
-                document.getElementById('btn2').checked = ${readStr[1]};
-
-                if(document.getElementById('btn1').checked) {
-                  document.getElementById('label1').innerHTML = 'on';
-                } else {
-                  document.getElementById('label1').innerHTML = 'off';
-                }
-
-                if(document.getElementById('btn2').checked) {
-                  document.getElementById('label2').innerHTML = 'on';
-                } else {
-                  document.getElementById('label2').innerHTML = 'off';
-                }
-              }
-
-              // btn1의 상태값 변경시키는 EventListener
-              btn1.addEventListener('click', (event) => {    
-                if(document.getElementById('btn1').checked) {
-                  document.getElementById('label1').innerHTML = 'on'; 
-                  writtenStr[0] = "true";
-                } else {
-                  document.getElementById('label1').innerHTML = 'off'; 
-                  writtenStr[0] = "false";
-                }
-                document.getElementById('save').form.submit();            // checkbox 누를 때마다 상태 save
-              });
-              // btn2의 상태값 변경시키는 EventListener
-              btn2.addEventListener('click', (event) => {    
-                if(document.getElementById('btn2').checked) {
-                  document.getElementById('label2').innerHTML = 'on'; 
-                  writtenStr[1] = "true";
-                } else {
-                  document.getElementById('label2').innerHTML = 'off'; 
-                  writtenStr[1] = "false";
-                }
-                document.getElementById('save').form.submit();            // checkbox 누를 때마다 상태 save
-              });
-
-              // Javascript Function
-              function btn1ButtonClicked() {
-                document.getElementById('btn1').click();
-              }
-              function btn2ButtonClicked() {
-                document.getElementById('btn2').click();
-              }
-              //----------------------------JAVASCRIPT----------------------------
-            `);
             response.writeHead(200);
-            response.end(template);
+            response.end(loadHTML(readStr));
       });
     } else if(pathname === '/create'){      // submit 버튼 누를 시 동작
       var body = '';
