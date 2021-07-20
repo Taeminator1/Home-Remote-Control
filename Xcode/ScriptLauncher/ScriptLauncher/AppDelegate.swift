c5dfdb169a69f0678a95a69628979e3bc92169e3
Taemin <dbsxoals689@naver.com>
2021-07-21 01:24:18 +0900

    Xcode 프로젝트 분리

---
+++ Xcode/ScriptLauncher/ScriptLauncher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ScriptLauncher
+//
+//  Created by Taemin Yun on 7/21/21.
+//  Copyright © 2021 Taemin Yun. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcode/ScriptLauncher/ScriptLauncher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScriptLauncher
+//
+//  Created by Taemin Yun on 7/21/21.
+//  Copyright © 2021 Taemin Yun. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
