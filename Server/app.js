869065ddf2b8ea6502fd5e6397a5dc2415cd4555
Taemin <dbsxoals689@naver.com>
2021-07-28 02:15:03 +0900

    리펙터링

--- Server/app.js
+++ Server/app.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-const inPort = ;            // 내부 포트 번호
+const inPort = 0;            // 내부 포트 번호
  
 function loadHTML(readStr) {
   return `
@@ -11,75 +11,54 @@ function loadHTML(readStr) {
     <html>
     <head>
       <title>Hmoe Remote Contorl</title>
-      <meta charset="utf-8">
+      <meta charset='utf-8'>
     </head>
     <body>
-      <form action="/create" method="post">
+      <form action='/create' method='post'>
         <h3>Test Buttons</h3>
         <ul>
           <li>
-            <label><input type="checkbox" name="test1" id="btn1"> button1 </label>
-            <label id = "label1"> ${readStr[0]} </label>
+            <label><input type='checkbox' name='test1' id='btn1'> button1 </label>
+            <label id = 'label1'> ${readStr[0]} </label>
           </li>
           <li>
-            <label><input type="checkbox" name="test2" id="btn2"> button2 </label>
-            <label id = "label2"> ${readStr[1]} </label>
+            <label><input type='checkbox' name='test2' id='btn2'> button2 </label>
+            <label id = 'label2'> ${readStr[1]} </label>
           </li>
-            <p><input type="hidden" id="save"></p>
+            <p><input type='hidden' id='save'></p>
         </ul>
       </form>
-      <script>
-        var writtenStr = ["", ""];
-        writtenStr[0] = ${readStr[0]};
-        writtenStr[1] = ${readStr[1]};
 
-        // 웹페이지가 로드될 때 실행
-        window.onload = function() {
+      <script>
+        window.onload = function() {                          // 웹페이지가 로드될 때 실행
           document.getElementById('btn1').checked = ${readStr[0]};
           document.getElementById('btn2').checked = ${readStr[1]};
 
-          if(document.getElementById('btn1').checked) {
-            document.getElementById('label1').innerHTML = 'on';
-          } else {
-            document.getElementById('label1').innerHTML = 'off';
-          }
-
-          if(document.getElementById('btn2').checked) {
-            document.getElementById('label2').innerHTML = 'on';
-          } else {
-            document.getElementById('label2').innerHTML = 'off';
-          }
+          setHTMLState('label1', 'btn1')
+          setHTMLState('label2', 'btn2')
         }
 
-        // btn1의 상태값 변경시키는 EventListener
-        btn1.addEventListener('click', (event) => {    
-          if(document.getElementById('btn1').checked) {
-            document.getElementById('label1').innerHTML = 'on'; 
-            writtenStr[0] = "true";
-          } else {
-            document.getElementById('label1').innerHTML = 'off'; 
-            writtenStr[0] = "false";
-          }
-          document.getElementById('save').form.submit();            // checkbox 누를 때마다 상태 save
-        });
-        // btn2의 상태값 변경시키는 EventListener
-        btn2.addEventListener('click', (event) => {    
-          if(document.getElementById('btn2').checked) {
-            document.getElementById('label2').innerHTML = 'on'; 
-            writtenStr[1] = "true";
-          } else {
-            document.getElementById('label2').innerHTML = 'off'; 
-            writtenStr[1] = "false";
-          }
-          document.getElementById('save').form.submit();            // checkbox 누를 때마다 상태 save
-        });
-
-        // Javascript Function
-        function btn1ButtonClicked() {
-          document.getElementById('btn1').click();
+        // btn의 상태값 변경시키는 EventListener 생성
+        setEventListener(btn1, 'btn1', 'label1');
+        setEventListener(btn2, 'btn2', 'label2');
+        
+        function setEventListener(btn, btnId, labelId) {
+          btn.addEventListener('click', (event) => {    
+            setHTMLState(labelId, btnId)
+            saveHTMLState('save')                             // checkbox 누를 때마다 상태 save
+          });
         }
-        function btn2ButtonClicked() {
-          document.getElementById('btn2').click();
+
+        function setHTMLState(labelId, btnId) {
+          document.getElementById(labelId).innerHTML = document.getElementById(btnId).checked ? 'on' : 'off';
+        }
+
+        function saveHTMLState(id) {
+          document.getElementById(id).form.submit();
+        }
+       
+        function buttonClicked(btn) {     // iOS 앱에서 접근 가능하도록 함수 정의
+          document.getElementById(btn).click();
         }
       </script>
     </body>
@@ -90,55 +69,53 @@ function loadHTML(readStr) {
 var app = http.createServer(function(request,response){
   var baseURL = 'http://' + request.headers.host + '/';
   var pathname = new URL(request.url, baseURL).pathname
-  var title = "data.txt";
+  var title = 'data.txt';
 
   if(pathname === '/'){           // URL에 주소 입력시 동작
-    if (!fs.existsSync('data.txt')) {     // data.txt 파일이 존재하지 않으면 파일 생성
+    if (!fs.existsSync(`${title}`)) {     // data.txt 파일이 존재하지 않으면 파일 생성
       fs.writeFile(`${title}`, 'false/false/', 'utf8', function(err){
         response.writeHead(302, {Location: `/?id=${title}`});
         response.end();
       });
     }
     else {                                // data.txt 파일이 존재하면 파일 읽기
-      fs.readFile('data.txt', 'utf8', function(err, description){
-        var readStr = description.split("/");
-        console.log("Success reading file")
+      fs.readFile(`${title}`, 'utf8', function(err, description){
+        var readStr = description.split('/');
+        console.log('Success reading file')
         response.writeHead(200);
         response.end(loadHTML(readStr));
       });
     }
-  } else if(pathname === '/create'){      // submit 버튼 누를 시 동작
+  } 
+  else if(pathname === '/create'){      // submit 버튼 누를 시 동작
     var body = '';
     request.on('data', function(data){
       body = body + data;
     });
     request.on('end', function(){
       var post = qs.parse(body);
-      var description = "";
 
-      if(post.test1 == "on") {
-        description = description + "true/";
-      } else {
-        description = description + "false/";
-      }
-      if(post.test2 == "on") {
-        description = description + "true/";
-      } else {
-        description = description + "false/";
-      }
+      var description = '';
+      description += saveState(post.test1)
+      description += saveState(post.test2)
       
       fs.writeFile(`${title}`, description, 'utf8', function(err){
         response.writeHead(302, {Location: `/?id=${title}`});
         response.end();
-        console.log("Success writing file")
+        console.log('Success writing file')
       });
     });
-  } else {
+  } 
+  else {
     response.writeHead(404);
     response.end('Not found');
   }
 });
 
 app.listen(inPort, function() {
-  console.log("HRC node.js is running on port %d", inPort);
+  console.log('HRC node.js is running on port %d', inPort);
 });
+
+function saveState(buttonState) {
+  return buttonState == 'on' ? 'true/' : 'false/';
+}
