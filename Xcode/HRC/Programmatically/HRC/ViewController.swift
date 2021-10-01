dcddf52e970bc1ce9199f27503f6ec5b4ec73e59
Taemin <dbsxoals689@naver.com>
2021-10-01 21:17:27 +0900

    각 Cell에 임시로 내용 추가

--- Xcode/HRC/Programmatically/HRC/ViewController.swift
+++ Xcode/HRC/Programmatically/HRC/ViewController.swift
@@ -11,6 +11,10 @@ class ViewController: UIViewController {
     
     var tableView = UITableView()
     let tableViewCellId = "tableViewCellId"
+    private let sections: [String] = ["Network", "Controls", "Test"]
+    private let networkSection: [String] = ["Status"]
+    private let controlsSection: [String] = ["Close the window", "Turn on the airconditioner"]
+    private let testSection: [String] = ["", ""]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +29,7 @@ class ViewController: UIViewController {
     
     func setupTableView() {
         tableView.dataSource = self
+        tableView.dataSource = self
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: tableViewCellId)
         tableView.tableFooterView = UIView()
@@ -34,12 +39,41 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].uppercased()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        if section == 0 {
+            return networkSection.count
+        }
+        else if section == 1 {
+            return controlsSection.count
+        }
+        else if section == 2 {
+            return testSection.count
+        }
+        else {
+            return 0
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: tableViewCellId, for: indexPath)
+        
+        if indexPath.section == 0 {
+            cell.textLabel?.text = "\(networkSection[indexPath.row])"
+        }
+        else if indexPath.section == 1 {
+            cell.textLabel?.text = "\(controlsSection[indexPath.row])"
+        }
+        else if indexPath.section == 2 {
+            cell.textLabel?.text = "\(testSection[indexPath.row])"
+        }
         
         return cell
     }
