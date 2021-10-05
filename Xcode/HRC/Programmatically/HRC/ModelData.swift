9c7ee049192a8eaf8e134217d87b7786669c3b31
Taemin <dbsxoals689@naver.com>
2021-10-05 22:26:26 +0900

    Json 형식의 데이터를 반환받기 위한 모델 추가

---
+++ Xcode/HRC/Programmatically/HRC/ModelData.swift
@@ -0,0 +1,50 @@
+//
+//  ModelData.swift
+//  HRC
+//
+//  Created by 윤태민 on 10/5/21.
+//
+
+import Foundation
+import UIKit
+
+struct Section: Codable {
+    var title: String
+    var contents: [String]
+}
+
+struct ModelData {
+    static let stringData = """
+        [
+            {
+                "section": "Network",
+                "contents": [
+                    "Status"
+                ]
+            },
+            {
+                "section": "Controls",
+                "contents": [
+                    "Close the window",
+                    "Turn on the airconditioner"
+                ]
+            }
+        ]
+    """
+    
+    static func getSectionData() -> [Section] {
+        var sections: [Section] = []
+        
+        if let jsonData = stringData.data(using: .utf8) {
+            let decoder = JSONDecoder()
+            do {
+                sections = try decoder.decode([Section].self, from: jsonData)
+                print(sections)
+            }
+            catch {
+            }
+        }
+        
+        return sections
+    }
+}
